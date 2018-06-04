e805b2cfaba76dd725744eef921cd2388a2d567c
thieunv <thieunguyenvan@admicro.vn>
2018-06-04 14:55:14 +0700

    update

---
+++ crawl_accuweather/__init__.py
@@ -0,0 +1,1 @@
+name = "crawl_accuweather"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_pkg",
+    version="0.0.1",
+    author="Thieu Nguyen",
+    author_email="thieunguyen287@gmail.com",
+    description="Accuweather crawler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thieunguyen287/crawl_accuweather",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
